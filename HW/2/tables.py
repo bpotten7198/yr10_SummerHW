4dd1079c0376bb945e9ee46273235649bb1b7419
bpotten7198 <bpotten7198@sohamcollege.org.uk>
2014-06-19 23:01:57 +0100

    Challenge finished

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,12 @@
 #paste code here
+### Time table program v1.0 ###
+def tTP(amount,num):
+    aT = 0
+    for x in range(amount+1):
+        print("{0} * {1} = {2}".format(num,aT,aT*num))
+        aT+=1
+
+amount1 = int(input("How many times will it loop? ")) 
+num2 = int(input("Enter the num to times: "))
+tTP(amount1,num2) 
+### End of program ###
